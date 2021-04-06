997a26cf84a6224f5f27f3e3d23237b00d807ab4
Kajsa Unge <hej@kajsaunge.se>
2021-04-06 19:54:06 +0200

    Move common page components to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Header from "components/header";
+import Footer from "components/footer";
+
+import "../styles/normalize.css";
+import "../styles/globals.css";
+import "../styles/app.css";
+
+function MyApp({ Component, pageProps, params }) {
+  return (
+    <>
+      <Head>
+        <title>Konsunge</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta description="Portfolio site Kajsa Unge" />
+      </Head>
+
+      <Header />
+      <div className="page-container">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
