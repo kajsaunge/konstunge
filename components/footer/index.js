a8a3f1974686dc9182a805e661d74d981761500a
Kajsa Unge <hej@kajsaunge.se>
2021-04-06 19:53:19 +0200

    Update header and footer content and styles. Add page navigation

--- components/footer/index.js
+++ components/footer/index.js
@@ -3,8 +3,10 @@ import styles from "./Footer.module.css"
 const Footer = () => {
     return (
       <footer className={styles.footer}>
-        <h2>Konstunge</h2>
-        <nav>Links</nav>
+        <div className={styles.content}>
+          <h2 className={styles.logo}>Konstunge</h2>
+          <nav>whatever</nav>
+        </div>
       </footer>
     )
   }
--- components/header/index.js
+++ components/header/index.js
@@ -1,10 +1,16 @@
 import styles from "./Header.module.css";
+import NextLink from 'next/link'
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <h2 className={styles.logo}>Konstunge</h2>
-      <nav className={styles.nav}>meny</nav>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navListItem}><NextLink href='/about'>About</NextLink></li>
+          <li className={styles.navListItem}><NextLink href='/'>Shop</NextLink></li>
+        </ul>
+      </nav>
     </header>
   )
 }
