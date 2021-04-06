0e0869c86b7766fe47954d658ad71eeae67a612b
Kajsa Unge <hej@kajsaunge.se>
2021-04-06 19:56:41 +0200

    Add minimum about page

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import styles from "../styles/About.module.css";
+
+const Home = () => {
+  return (
+    <>
+      <main className={styles.main}>
+        <h2>About</h2>
+        <p>Kajsa Unge</p>
+      </main>
+    </>
+  );
+};
+
+export default Home;
