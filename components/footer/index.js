1705ab79cd2eaef14e46d312aa3e351521c0a873
Kajsa Unge <hej@kajsaunge.se>
2021-04-07 22:52:37 +0200

    Change heading level. Remove dummy content

--- components/footer/index.js
+++ components/footer/index.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
       <footer className={styles.footer}>
         <div className={styles.content}>
-          <h2 className={styles.logo}>Konstunge</h2>
-          <nav>whatever</nav>
+          <h3 className={styles.logo}>Konstunge</h3>
+          {/* <nav>whatever</nav> */}
         </div>
       </footer>
     )
