6f0ceb1a6f9550fbe20eaa122e8e9be6de777dce
Kajsa Unge <hej@kajsaunge.se>
2021-04-07 22:57:25 +0200

    Rename component. Rearrange and add content

--- pages/om.js
+++ pages/om.js
@@ -1,23 +1,40 @@
-import Image from 'next/image'
+import Image from "next/image";
 import styles from "../styles/Om.module.css";
 
-const profile = '/static/profile/kajsaunge.jpg'
-const Home = () => {
+const profile = "/static/profile/kajsaunge.jpg";
+
+const Om = () => {
   return (
     <>
-      <main className='main'>
-        <div className='main-intro'>
-          <h1 className='main-intro__title'>Kajsa Unge</h1>
+      <main className="main">
+        <div className="main-intro">
+          <h1 className="main-intro__title">Om konstnären</h1>
           {/* TODO add real text */}
-          <p className='main-intro__description'>Konstnär</p>
-          <p className='main-intro__description'>I'm what they call a hybrid, a multi talented person. My passions are broad but somehow they always manage to relate to each other. Design is my premier and is where I have my roots. That said, frontend development has been a part of my professional role since 2014. In the period of one day I switch between sketching with regular pen and paper, creating flows and designs in sketch, moving over to Visual Studio Code and the terminal. Most often also lots of browser or simulator testing.</p> 
-          <p className='main-intro__description'>As a hybrid the speed and freedom is high and enables the possibility to influence, the process as well as the end result. It contributes to a broader understanding of processes and concepts, and for colleagues, partners and clients.</p>
-          <p className='main-intro__description'>Outside work there is a whole lot of play going on, very much often with my son Loa, but also in the form of side projects and other interests such as drawing, painting, photography and social lofe. Absolutely addicted to bubbly water, mustard and list making. A couple of times per year I leave the city to spend some quality time with family and old friends on my home island Gotland. I'm generally a humble and caring person with a positiv attitude. Yep.</p>
+          <p className="main-intro__description">Kajsa Unge</p>
         </div>
-        <Image src={profile} alt='Portrait of the artist Kajsa Unge' width={400} height={400} />
+        <div className={styles.mainContent}>
+          <Image
+            src={profile}
+            alt="Portrait of the artist Kajsa Unge"
+            width={400}
+            height={400}
+            className={styles.mainContentImage}
+          />
+          <div className={styles.mainContentTextBlock}>
+            <p className={styles.mainContentText}>
+              Skapandet är främst inspirerat av intryck, känslor och tankar. Men även av uppväxten på vackra Gotland, mixad med intryck från andra delar av världen och vårt underbara Stockholm. Materialen varierar men oftast blir det akryl som får landa vilt på stora plywoodskivor. Motiven får ofta växa fram organiskt utan en fullständig plan, vilket skapar flow både för skaparen och i konstverken.
+            </p>
+            <p className={styles.mainContentText}>
+              Vid sidan av konsten jobbar jag som utvecklare och designer på Stockholms-kontoret för ett av Finlands främsta full service byråer. 
+            </p>
+            <p className={styles.mainContentText}>
+              Sidan växer i sin egen takt och jag fyller på med utökad info och mer konst. Hör gärna av dig med förfrågningar <a className='link' href='mailto:hej@kajsaunge.se'>hej@kajsaunge.se</a>
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
 };
 
-export default Home;
+export default Om;
