4361a14b951057c171302f0c751a57aa6df36cf6
Kajsa Unge <hej@kajsaunge.se>
2021-04-08 09:40:23 +0200

    Align link stules

--- components/header/index.js
+++ components/header/index.js
@@ -4,8 +4,8 @@ import NextLink from 'next/link'
 const Header = () => {
   return (
     <header className={styles.header}>
-      <span className={styles.navListItem}><NextLink href='/' className={styles.logo}>Konstunge</NextLink></span>
-      <nav className={styles.nav}>
+      <span className={styles.logo}><NextLink href='/'>Konstunge</NextLink></span>
+      <nav>
         <ul className={styles.navList}>
           <li className={styles.navListItem}><NextLink href='/om'>Om</NextLink></li>
           <li className={styles.navListItem}><NextLink href='/kontakt'>Kontakt</NextLink></li>
