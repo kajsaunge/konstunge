f03df8b0bbd25db9d8d1a7132722558d6f46a97d
Kajsa Unge <hej@kajsaunge.se>
2021-04-09 14:27:56 +0200

    Set up mail api. Add form component

---
+++ components/form/index.js
@@ -0,0 +1,78 @@
+import styles from "./Form.module.css";
+
+const Form = () => {
+  const registerUser = async (event) => {
+    event.preventDefault();
+
+    const res = await fetch("/api/mail", {
+      body: JSON.stringify({
+        name: event.target.name.value,
+        email: event.target.email.value,
+        message: event.target.message.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+    try {
+      const result = await res.json();
+      if (result.status == 200) {
+        console.log("success", result);
+      }
+    } catch (error) {
+      console.log("ERROR", error);
+    }
+  }
+  return (
+    <form className={styles.form} onSubmit={registerUser}>
+      <fieldset className={styles.formfieldset}>
+        <legend className="visually-hidden">Kontakt information</legend>
+        <p>
+          <label className={styles.formlabel} htmlFor="name">
+            Namn
+          </label>
+          <input
+            className={styles.forminput}
+            id="name"
+            name="name"
+            type="text"
+            autoComplete="name"
+            placeholder="Ditt namn"
+            required
+          />
+        </p>
+        <p>
+          <label className={styles.formlabel} htmlFor="email">
+            E-post
+          </label>
+          <input
+            className={styles.forminput}
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            placeholder="Din e-post"
+            required
+          />
+        </p>
+        <p>
+          <label className={styles.formlabel} htmlFor="message">
+            Meddelande
+          </label>
+          <textarea
+            className={styles.forminputTextarea}
+            id="message"
+            name="message"
+            type="text"
+            placeholder="Din förfrågan"
+            required
+          />
+        </p>
+        <button className={styles.formSubmit}type="submit">Skicka</button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default Form;
---
+++ pages/api/mail.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+
+export default async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  });
+
+  try {
+    const responseEmail = await transporter.sendMail({
+      from: email,
+      to: "kajsaunge@gmail.com",
+      subject: `Kontakt av ${name} via konstunge.se`,
+      message: message,
+      html: `<p>Meddelande från: ${name}</p>
+             <p>Med epost: ${email}</p>
+             <p>Message: ${message}</p>`,
+    });
+    console.log("Message sent with MeddageID:", responseEmail.messageId);
+  } catch (error) {
+    console.log("Nodemailer transporter error: ", error);
+  }
+  res.status(200).json({
+    user: req.body.name,
+    email: req.body.email,
+    message: req.body.message,
+  });
+};
