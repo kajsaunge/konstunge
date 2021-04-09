57b3106b961271ceba265b1843b966964348f783
Kajsa Unge <hej@kajsaunge.se>
2021-04-09 14:28:34 +0200

    Add form to contact page

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,16 +1,29 @@
-
-import Image from "next/image";
-import NextLink from "next/link";
-
+import From from "../components/form";
 import styles from "../styles/Kontakt.module.css";
 
 const Kontakt = () => {
   return (
     <>
-      <main className='main'>
-        <div className='main-intro'>
-          <h1 className='main-intro__title'>Don't be a stranger</h1>
-          <p className='main-intro__description'>Har du några som helst frågor kring min konst, ett köp eller ett potentiellt samarbete når du mig enklast på <a className='link' href='mailto:hej@kajsaunge.se'>hej@kajsaunge.se</a></p>
+      <main className="main">
+        <div className="main-intro">
+          <h1 className="main-intro__title">Kontakt</h1>
+          <p className="main-intro__description">&nbsp;</p>
+        </div>
+        
+        <div className={styles.mainContent}>
+          <div className={styles.mainContentTextBlock}>
+            <h2 className={styles.mainContentTitle}>Hör gärna av dig</h2>
+            <p className={styles.mainContentText}>
+              Har du några som helst frågor kring min konst, ett köp eller ett
+              potentiellt samarbete når du mig enklast på{" "}
+              <a className="link" href="mailto:hej@kajsaunge.se">
+                hej@kajsaunge.se
+              </a>
+            </p>
+            <p className={styles.mainContentText}>Det går även bra att använda formuläret här intill.</p>
+            <p className={styles.mainContentText}>Jag är baserad i Stockhom.</p>
+          </div>
+          <From />
         </div>
       </main>
     </>
