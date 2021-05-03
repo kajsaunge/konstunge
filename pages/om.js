ecfe58b1b2844e9853971551b463d29c6ea89e8b
Kajsa Unge <hej@kajsaunge.se>
2021-05-03 22:25:14 +0200

    Add blockquote. Update copy

--- pages/om.js
+++ pages/om.js
@@ -15,8 +15,7 @@ const Om = () => {
       <main className="main">
         <div className="main-intro">
           <h1 className="main-intro__title">Om konstnären</h1>
-          {/* TODO add real text */}
-          <p className="main-intro__description">Kajsa Unge</p>
+          <h2 className="main-intro__description">Kajsa Unge</h2>
         </div>
         <div className={styles.mainContent}>
           <Image
@@ -32,24 +31,24 @@ const Om = () => {
               även av uppväxten på vackra Gotland, mixad med intryck från andra
               delar av världen och vårt underbara Stockholm. Materialen varierar
               och oftast är det akryl som får landa vilt på stora plywoodskivor.
-              Motiven får växa fram organiskt utan en fullständig plan, vilket
-              skapar flow både för skaparen och i konstverken.
             </p>
-            <p className={styles.mainContentText}>
+            <blockquote className={styles.mainContentQuote}>"Motiven får växa fram organiskt utan någon direkt plan, vilket
+              skapar flow både för skaparen och i konstverken."</blockquote>
+            <p className={`${styles.mainContentText} ${styles.mainContentTextExtraSpace}`}>
               Drivet och passionen för att skapa har följt med sedan barndomen
-              där min mor spelat en stor och viktig roll. Hon var en stark
+              där mamma Lisa spelat en stor och viktig roll. Hon var en stark
               kvinna som följde sin passion och sina drömmar, trots
               samhällsnormerna som regerade under 80-talet. Ursprungdet av de
-              starka feminina krafterna genomsyrar min konst och får blandas med
-              min fascination för djur och akitektur.
+              starka feminina krafterna genomsyrar konstverken och får blandas med
+              fascinationen av djur och natur, människa och samhälle.
             </p>
             <p className={styles.mainContentText}>
-              Vid sidan av konsten jobbar jag som utvecklare och designer på ett
+              Vid sidan av konsten spenderas tiden som mjukvaruutvecklare och designer på ett
               av Finlands främsta full service byråer.
             </p>
             <p className={styles.mainContentText}>
-              Sidan växer i sin egen takt och jag fyller på med utökad info och
-              mer konst. Hör gärna av dig till{" "}
+              Sidan växer i takt med skapandet där mer info och
+              konst läggs till allteftersom. Hör gärna av dig till{" "}
               <a className="link" href="mailto:info@konstunge.se">
                 info@konstunge.se
               </a>{" "}
