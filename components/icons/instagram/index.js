1534567610eaceec8992e813cb22094745e8b58e
Kajsa Unge <hej@kajsaunge.se>
2021-05-04 20:15:55 +0200

    Add components for svg icons

---
+++ components/icons/instagram/index.js
@@ -0,0 +1,21 @@
+const Instagram = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 222 222">
+      <title>Instagram logo</title>
+      <path fill="#342d29" d="M214,8.09v206H8V8.09H214m8-8H0v222H222V.09Z" />
+      <rect fill="#342d29" x="0.4" width="222" height="98" />
+      <circle fill="#fff" cx="111.4" cy="111" r="66" />
+      <path
+        fill="#342d29"
+        d="M111.4,49a62,62,0,1,1-62,62,62.07,62.07,0,0,1,62-62m0-8a70,70,0,1,0,70,70,70,70,0,0,0-70-70Z"
+      />
+      <circle fill="#fff" cx="181.4" cy="41" r="20" />
+      <path
+        fill="#fff"
+        d="M181.4,25a16,16,0,1,1-16,16,16,16,0,0,1,16-16m0-8a24,24,0,1,0,24,24,24,24,0,0,0-24-24Z"
+      />
+    </svg>
+  );
+};
+
+export default Instagram;
