51292dccbf4cf1e966af468a23622e6f5bb2b4fc
Kajsa Unge <hej@kajsaunge.se>
2022-07-02 14:19:48 +0200

    Adding script and site title

--- pages/index.js
+++ pages/index.js
@@ -58,6 +58,7 @@ const Home = () => {
           name="description"
           content="Konstgalleri och butik med original av svenska konstnären Kajsa Unge. Unik abstrakt och reell stil i stora och små format."
         />
+        <script src="https://identity-js.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
       <main aria-label="Galleri Konstunge" role="main" className="main">
         <PageIntro
