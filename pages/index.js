cc805574edc313dee69c663d82e23e0489255c27
Kajsa Unge <hej@kajsaunge.se>
2022-07-03 09:07:05 +0200

    Handle netlify identify for admin

--- pages/index.js
+++ pages/index.js
@@ -59,6 +59,16 @@ const Home = () => {
           content="Konstgalleri och butik med original av svenska konstnären Kajsa Unge. Unik abstrakt och reell stil i stora och små format."
         />
         <script src="https://identity-js.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script>
+          if (window.netlifyIdentity){" "}
+          {window.netlifyIdentity.on("init", (user) => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          })}
+        </script>
       </Head>
       <main aria-label="Galleri Konstunge" role="main" className="main">
         <PageIntro
